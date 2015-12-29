d54bfac500d9629a6031e83a1ece9c181a70c314
Jacques Supcik <jacques.supcik@hefr.ch>
2015-12-30 00:07:26 +0100

    Accept all requests

--- telecom-tower-web-proxy.go
+++ telecom-tower-web-proxy.go
@@ -17,6 +17,7 @@ const (
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
+	CheckOrigin:     func(r *http.Request) bool { return true },
 }
 
 type Bitmap [][]string
